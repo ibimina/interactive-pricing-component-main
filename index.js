d025e27b4d8d4a65ae7146db6ce71be30b60a468
ibimina <96787210+ibimina@users.noreply.github.com>
2022-05-29 14:52:41 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const views = document.querySelector(".views");
 const rangeSlider = document.querySelector("#range-slid");
-const price = document.querySelector(".month-price");
+const price = document.querySelectorAll(".month-price");
 const thumbSlider = document.querySelector('input[type ="checkbox"]');
 const thumbSlide = document.querySelector(".toggle-thumb::before");
-const year = document.querySelector(".mon");
+const year = document.querySelectorAll(".mon");
 
 function cal(amount) {
   let monthPrice = (amount * 12 * 0.75).toFixed(2);
@@ -25,49 +25,96 @@ function slide() {
   console.log(view);
   if (view === "0" && !thumbSlider.checked) {
     views.textContent = `10K`;
-    price.textContent = "$8.00";
+    price.forEach(element => {
+      element.textContent = "$8.00";
+    });
     year.textContent = "/ month";
   } else if (view === "1" && !thumbSlider.checked) {
     views.textContent = `50K`;
-    price.textContent = "$12.00";
-    year.textContent = "/ month";
+    price.forEach((element) => {
+      element.textContent = "$12.00";
+    });
+     year.forEach((element) => {
+       element.textContent = "/ month";
+     });
+   
   } else if (view === "2" && !thumbSlider.checked) {
     views.textContent = `100K`;
-    price.textContent = "$16.00";
-    year.textContent = " /month";
+    price.forEach((element) => {
+      element.textContent = "$16.00";
+    });
+     year.forEach((element) => {
+       element.textContent = "/ month";
+     });
+
   } else if (view === "3" && !thumbSlider.checked) {
     views.textContent = `500K`;
-    price.textContent = "$24.00";
-    year.textContent = "/ month";
+    price.forEach((element) => {
+      element.textContent = "$24.00";
+    });
+    year.forEach((element) => {
+      element.textContent = "/ month";
+    });
+
   } else if (view === "4" && !thumbSlider.checked) {
     views.textContent = `1M`;
-    price.textContent = "$36.00";
-    year.textContent = "/ month";
+    price.forEach((element) => {
+      element.textContent = "$36.00";
+    });
+    year.forEach((element) => {
+      element.textContent = "/ month";
+    });
+
   } else if (view === "0" && thumbSlider.checked) {
     views.textContent = `10K`;
     amount = 8;
-    price.textContent = `$${cal(amount)}`;
-    year.textContent = "/year";
+    price.forEach((element) => {
+      element.textContent = `$${cal(amount)}`;
+    });
+    year.forEach((element) => {
+      element.textContent = "/ year";
+    });
+
   } else if (view === "1" && thumbSlider.checked) {
     views.textContent = `50K`;
     amount = 12;
-    price.textContent = `$${cal(amount)}`;
-    year.textContent = "/year";
+     price.forEach((element) => {
+       element.textContent = `$${cal(amount)}`;
+     });
+     year.forEach((element) => {
+       element.textContent = "/ year";
+     });
+
   } else if (view === "2" && thumbSlider.checked) {
     views.textContent = `100K`;
     amount = 16;
-    price.textContent = `$${cal(amount)}`;
-    year.textContent = "/year";
+     price.forEach((element) => {
+       element.textContent = `$${cal(amount)}`;
+     });
+     year.forEach((element) => {
+       element.textContent = "/ year";
+     });
+
   } else if (view === "3" && thumbSlider.checked) {
     views.textContent = `500K`;
     amount = 24;
-    price.textContent = `$${cal(amount)}`;
-    year.textContent = "/year";
+     price.forEach((element) => {
+       element.textContent = `$${cal(amount)}`;
+     });
+     year.forEach((element) => {
+       element.textContent = "/ year";
+     });
+  
   } else if (view === "4" && thumbSlider.checked) {
     views.textContent = `1M`;
     amount = 36;
-    price.textContent = `$${cal(amount)}`;
-    year.textContent = "/year";
+     price.forEach((element) => {
+       element.textContent = `$${cal(amount)}`;
+     });
+     year.forEach((element) => {
+       element.textContent = "/ year";
+     });
+
   }
 }
 
